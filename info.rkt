b7009fe07b609888399f84defb9ba0ebe06f7612
Sam Tobin-Hochstadt <samth@ccs.neu.edu>
2014-12-15 21:30:43 -0500

    Remove unneeded dependency.

--- info.rkt
+++ info.rkt
@@ -2,8 +2,7 @@
 (define collection 'multi)
 (define deps '("base"
                "compiler-lib"
-               "scheme-lib"
-               "rackunit-lib"))
+               "scheme-lib"))
 
 (define pkg-desc "Tools for managing C extensions, such as `raco ctool`")
 
